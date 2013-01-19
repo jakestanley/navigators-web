8832f8b64506bc10ff1a8975331ab481f1ef6ad6
Jake Stanley <prudentia713@gmail.com>
2013-01-19 03:19:09 +0000

    Removed unused imports from main class.

--- SpaceCommanders/src/begin/GameStart.java
+++ SpaceCommanders/src/begin/GameStart.java
@@ -1,9 +1,6 @@
 package begin;
 
 import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
 import ships.Ship;
