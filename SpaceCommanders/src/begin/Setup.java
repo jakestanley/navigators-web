2b4ff5dcc39b8e445777286da1bca2516d158bf2
Jake Stanley <prudentia713@gmail.com>
2013-02-03 06:19:09 +0000

    Removed unnecessary array lists from Setup
    Didn't need lists for any of the Components

--- SpaceCommanders/src/begin/Setup.java
+++ SpaceCommanders/src/begin/Setup.java
@@ -7,21 +7,19 @@ import ships.Ship;
 import ships.Weapon;
 import tools.Coordinate;
 
-
 public class Setup {
 	
 	int shipIndex = 0;
-	ArrayList<Ship> shipList = new ArrayList<Ship>();
-	ArrayList<Weapon> weaponList= new ArrayList<Weapon>();
-	ArrayList<Reactor> reactorList = new ArrayList<Reactor>();	
+	public ArrayList<Ship> shipList = new ArrayList<Ship>();
 
 	public Setup(boolean test){
 		if(test == true){
-			shipList.add(shipIndex, new Ship(shipIndex, "Interstellar 5555", new Coordinate(0, 0, 0)));
-			reactorList.add(shipIndex, new Reactor());
-			weaponList.add(shipIndex, new Weapon());
 			
+			shipList.add(shipIndex, new Ship(shipIndex, "Interstellar 5555", new Coordinate(0, 0, 0)));
 			shipList.get(shipIndex).printAllShipInfo();
+			shipIndex++;
+			
+			
 		} else {
 			// run usual game stuff
 		}
