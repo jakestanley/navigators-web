857d622e6606882ed8c117a565bbfc0bc893d7d0
Jake Stanley <prudentia713@gmail.com>
2013-02-09 18:38:52 +0000

    Moved Setup to main method in Game class

--- SpaceCommanders/src/begin/Game.java
+++ SpaceCommanders/src/begin/Game.java
@@ -21,26 +21,26 @@ public class Game extends BasicGame{
 		AppGameContainer game = new AppGameContainer(new Game("Space Commanders"));
 		game.setDisplayMode(800, 600, false);
 		game.setTargetFrameRate(60);
-		game.start();
-		
-	}
-	
-	@Override
-	public void render(GameContainer gc, Graphics g) throws SlickException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void init(GameContainer gc) throws SlickException {
-		// TODO Auto-generated method stub
-		
 		try {
 			setup = new Setup(0);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		//game.start();
+	}
+	
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		// TODO Auto-generated method stub
+		g.drawString(setup.client.getClientStatus(), 100, 100);
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+		// TODO Auto-generated method stub
+		
+		
 		
 	}
 
