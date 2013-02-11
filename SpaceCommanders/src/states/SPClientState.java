51fc48b2a4172068b227a3290c16c859f33cc8c7
Jake Stanley <prudentia713@gmail.com>
2013-02-11 10:26:47 +0000

    Created new states for different game options.

---
+++ SpaceCommanders/src/states/SPClientState.java
@@ -0,0 +1,69 @@
+package states;
+
+import org.lwjgl.input.Mouse;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class SPClientState extends BasicGameState {
+
+	int stateID;
+	String mousePosition = "No mouse input";
+	String mouseClickedOn = "No mouse clicks yet";
+	
+	public SPClientState(int state) {
+		stateID = state;
+	}
+
+
+	@Override
+	public void init(GameContainer gc, StateBasedGame sbg)
+			throws SlickException {
+		
+		// start GameClient from here
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+		g.drawString("Single Player.", 50, 50);
+		
+		g.drawString("Main Menu.", 50, 400);
+		g.drawString(mousePosition, 250, 10);
+		g.drawRect(45, 395, 100, 30);
+		
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg, int delta)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		int xPos = Mouse.getX();
+		int yPos = Mouse.getY();
+		mousePosition = ("Mouse position is: " +xPos + ", " + yPos);
+		if(Mouse.isButtonDown(Input.MOUSE_LEFT_BUTTON)){
+			if(xPos >= 45 && xPos <= 145 && yPos >= 55 && yPos <= 85){
+				
+				sbg.enterState(0);
+			} else {
+				
+			}
+			
+		}
+		
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return stateID;
+	}
+
+}
