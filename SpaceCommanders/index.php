597af8ede9c08439d3d5768eefdede969a072781
Jake Stanley <prudentia713@gmail.com>
2013-04-02 17:02:20 +0100

    Placeholder for new webby Space Commanders.

---
+++ SpaceCommanders/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<header>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<title>Space Commanders</title>
+Space Commanders.</header>
+<body>
+	Prepare yourself, Commander.
+</body>
+<footer></footer>
+</html>
